9b5bb878c63ff67e3522cb7757e0df11ba9614a4
Javan <Javan@Javans-MacBook-Pro.local>
2024-10-03 14:30:04 +0100

    turns game into async

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import pygame
 from game_context import GameContext
 from play_status import PlayStatus
@@ -5,42 +6,36 @@ from play_states import credits_state, end_state, main_menu_state, gameplay_stat
 
 pygame.init()
 
-FPS = pygame.time.Clock()
-GameContext.build_screen()
-
-def update_display():
-    pygame.display.update()
-    FPS.tick(60)
-
-def main():
+async def main():
     while True:
+        await asyncio.sleep(0)  # Yield control to the event loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
         if GameContext.PLAY_STATE == PlayStatus.CREDITS:
-            credits_state()
+            await credits_state()
 
         elif GameContext.PLAY_STATE == PlayStatus.MAIN_MENU:
-            main_menu_state()
+            await main_menu_state()
 
         elif GameContext.PLAY_STATE == PlayStatus.GAME_END:
-            end_state()
+            await end_state()
 
         elif GameContext.PLAY_STATE == PlayStatus.GAMEPLAY:
-            score = gameplay_state()  # Retrieve the score when gameplay ends
+            score = await gameplay_state()  # Retrieve the score when gameplay ends
 
         elif GameContext.PLAY_STATE == PlayStatus.GAME_OVER:
             try:
                 score
-                game_over_state(score)
-            except: game_over_state()  # Pass the score to the game over state
+                await game_over_state(score)
+            except: await game_over_state()  # Pass the score to the game over state
         elif GameContext.PLAY_STATE == PlayStatus.TUTORIAL:
-            tutorial_state()
+            await tutorial_state()
 
         elif GameContext.PLAY_STATE == PlayStatus.END_CREDITS:
-            end_credits_state()
+            await end_credits_state()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
